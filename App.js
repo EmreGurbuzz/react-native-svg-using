95916c048a791fb83e00f95e3c5590c908025911
Emre Gürbüz <emre_gurbuz@yahoo.com>
2021-10-30 01:24:45 +0300

    Add files via upload
    Svg test için computer.svg yüklendi. Yüklenen svg içindeki xml koddan fill kısmı kaldırıldı. App.js içerisine fill props olarak gönderildi.

---
+++ App.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Computer from './app/computer.svg';
+const App = () => {
+  return (
+    <Computer width="100%" height="100%" fill="red" />
+  );
+}
+export default App;
